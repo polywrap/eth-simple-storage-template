87cddeea9fc1a770f0a781ce02dd1b9b9f470df9
Niraj Kamdar <nirajkamdar27@pm.me>
2023-02-15 17:17:58 +0400

    fix: client configs

--- wrapper/assemblyscript/client-config.ts
+++ wrapper/assemblyscript/client-config.ts
@@ -1,22 +1,12 @@
 import { ClientConfig, PolywrapClientConfig } from "@polywrap/client-js";
 import { ethereumPlugin, Connections, Connection } from "@polywrap/ethereum-plugin-js";
-import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
-
 // USE THIS FOR LOCAL ETHEREUM NODE
 const ETH_PROVIDER = "http://127.0.0.1:8545/";
-
-// USE THIS FOR LOCAL IPFS NODE
-const IPFS_PROVIDER = "http://127.0.0.1:5001";
-
 
 function getPlugins(): ClientConfig["plugins"] {
   const connection = new Connection({ provider: ETH_PROVIDER })
   const connections = new Connections({ networks: { testnet: connection }, defaultNetwork: "testnet" });
   return [
-      {
-        uri: "wrap://ens/ipfs.polywrap.eth",
-        plugin: ipfsPlugin({}),
-      },
       {
         uri: "wrap://ens/ethereum.polywrap.eth",
         plugin: ethereumPlugin({ connections }),
@@ -28,12 +18,6 @@ export async function getClientConfig(
   _: Partial<PolywrapClientConfig>
 ): Promise<Partial<PolywrapClientConfig>> {
   return {
-    envs: [{
-      uri: "wrap://ens/ipfs.polywrap.eth",
-      env: {
-        provider: IPFS_PROVIDER,
-      }
-    }],
     plugins: getPlugins()
   }
 }
--- wrapper/rust/client-config.ts
+++ wrapper/rust/client-config.ts
@@ -1,21 +1,13 @@
 import { ClientConfig, PolywrapClientConfig } from "@polywrap/client-js";
-import { ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
 import { ethereumPlugin, Connections, Connection } from "@polywrap/ethereum-plugin-js";
-import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
-import { providers, ensAddresses } from "@polywrap/test-env-js";
+
+// USE THIS FOR LOCAL ETHEREUM NODE
+const ETH_PROVIDER = "http://127.0.0.1:8545/";
 
 function getPlugins(): ClientConfig["plugins"] {
-  const connection = new Connection({ provider: providers.ethereum })
+  const connection = new Connection({ provider: ETH_PROVIDER })
   const connections = new Connections({ networks: { testnet: connection }, defaultNetwork: "testnet" });
   return [
-      {
-        uri: "wrap://ens/ipfs.polywrap.eth",
-        plugin: ipfsPlugin({}),
-      },
-      {
-        uri: "wrap://ens/ens-resolver.polywrap.eth",
-        plugin: ensResolverPlugin({ addresses: { testnet: ensAddresses.ensAddress } }),
-      },
       {
         uri: "wrap://ens/ethereum.polywrap.eth",
         plugin: ethereumPlugin({ connections }),
@@ -27,12 +19,6 @@ export async function getClientConfig(
   _: Partial<PolywrapClientConfig>
 ): Promise<Partial<PolywrapClientConfig>> {
   return {
-    envs: [{
-      uri: "wrap://ens/ipfs.polywrap.eth",
-      env: {
-        provider: providers.ipfs
-      }
-    }],
     plugins: getPlugins()
   }
 }
